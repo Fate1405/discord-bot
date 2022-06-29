9329ba9956886ebd737e36986a868698c0bd1b6a
Fate1405 <jaipalpillay@icloud.com>
2022-06-29 10:41:21 +0200

    remove vulnerabilities

--- bot.js
+++ bot.js
@@ -1,15 +1,28 @@
 const { Client, Intents } = require('discord.js');
 require('dotenv').config()
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
-client.on("message", function (msg) {
-    if (msg.author.id == 172002275412279296) {
-        msg.reply("@everyone");
+client.on('voiceStateUpdate', (oldState, newState) => {
+    let oldSize;
+    let newSize;
+    if (oldState.channel === null) {
+        oldSize = 0;
+    } else {
+        oldSize = oldState.channel.members.size;
+    }
+    if (newState.channel === null) {
+        newSize = 0;
+    } else {
+        newSize = newState.channel.members.size;
+    }
+    if (newSize > oldSize) {
+        console.log(`New Size: ${newState.channel.members.size}\nOld Size: ${oldSize}`);
+        client.channels.cache.get('960866688688091212').send('@everyone someone joined a VC');
     }
 });
 
